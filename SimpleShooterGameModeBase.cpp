de76e8682b7e6ca93d5b4b75b58f848bd12b00b8
hongyeol.kim.lux <hongyeol.kim.lux@gmail.com>
2022-11-12 11:49:29 +0900

    코드 추가

---
+++ SimpleShooterGameModeBase.cpp
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+
+#include "SimpleShooterGameModeBase.h"
+
+ASimpleShooterGameModeBase::ASimpleShooterGameModeBase()
+{
+
+}
+
+void ASimpleShooterGameModeBase::PawnKilled(APawn* PawnKilled)
+{
+}
